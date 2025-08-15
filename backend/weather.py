8b0a3a2cd3a030ff88b93535436d7c75bcc45c16
Hari-Gian <harigian545@gmail.com>
2025-08-15 15:37:37 +0200

    weather api logik fertig

--- backend/weather.py
+++ backend/weather.py
@@ -5,3 +5,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
+
+def Weather(city):
+    base_url = "https://api.openweathermap.org/data/2.5/weather" 
+    params = {
+        "q" : city,             #der Stadtname
+        "appid" : API_KEY,        
+        "units" : "metric"      #in welcher einheit es ist
+    } 
+
+    #anfrage an openweather schicken
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None 
